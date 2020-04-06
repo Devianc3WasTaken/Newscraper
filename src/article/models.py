137457aca03ffb10a2da781698e0f290e663488a
Anton Nyagolov <a.nyagolov@se18.qmul.ac.uk>
2020-04-06 14:25:52 +0100

    implemented an article app v2

--- src/article/models.py
+++ src/article/models.py
@@ -15,9 +15,13 @@ class Article(models.Model):
     image = models.ImageField()
     category = models.CharField(max_length=100)
     favourite = models.BooleanField(default=False)
+    date = models.DateTimeField()
+
     def __str__(self):
         return self.title
-
+    # OR THIS
+    def __str__(self):
+        return "Title: {0} \nImage: {1} \nSubtitle: {2} \nBody: {3} \nSource: {4} \nDate: {5} \nCategory: {6}".format(self.title, self.image, self.subtitle, self.body, self.source, self.date, self.category)
 
 
 
